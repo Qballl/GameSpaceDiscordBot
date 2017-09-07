ddca0d1b9f358756f3b15cf49efccbed9f2cf5b4
TJPlaysNow <pannebakertj@gmail.com>
2017-09-07 18:25:14 -0500

    Changed send message to update topic
    Instead of sending a message in #info now we just change the topic to
    show the user count.

--- Discord Bot - GameSpace/src/com/pzg/www/discord/main/GamerSpaceBot.java
+++ Discord Bot - GameSpace/src/com/pzg/www/discord/main/GamerSpaceBot.java
@@ -1,14 +1,20 @@
 package com.pzg.www.discord.main;
 
 import java.util.HashMap;
+import java.util.List;
+
 import com.pzg.www.discord.object.Bot;
+import com.pzg.www.discord.object.CommandMethod;
+import com.pzg.www.discord.object.Method;
 import com.pzg.www.games.Connect4;
+
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.member.UserJoinEvent;
 import sx.blah.discord.handle.impl.events.guild.member.UserLeaveEvent;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.handle.obj.Permissions;
 
 public class GamerSpaceBot {
 	
@@ -21,6 +27,17 @@ public class GamerSpaceBot {
 	public GamerSpaceBot(String token, String prefix) {
 		bot = new Bot(token, prefix);
 		registerBot();
+		
+		bot.addCommand(new CommandMethod("updateusers", Permissions.ADMINISTRATOR.toString(), new Method() {
+			@Override
+			public void method(IUser user, IChannel chan, IGuild guild, String label, List<String> args) {
+				for (IChannel channel : guild.getChannels()) {
+					if (channel.getName().equalsIgnoreCase("info")) {
+						channel.changeTopic("Current users: " + guild.getUsers().size());
+					}
+				}
+			}
+		}));
 	}
 	
 	public void registerBot() {
@@ -36,8 +53,7 @@ public class GamerSpaceBot {
 	public void userJoin(UserJoinEvent event) {
 		for (IChannel channel : event.getGuild().getChannels()) {
 			if (channel.getName().equalsIgnoreCase("info")) {
-				channel.getMessageHistory(1).bulkDelete();
-				channel.sendMessage("Current users: " + event.getGuild().getUsers().size());
+				channel.changeTopic("Current users: " + event.getGuild().getUsers().size());
 			}
 		}
 	}
@@ -46,8 +62,7 @@ public class GamerSpaceBot {
 	public void userLeave(UserLeaveEvent event) {
 		for (IChannel channel : event.getGuild().getChannels()) {
 			if (channel.getName().equalsIgnoreCase("info")) {
-				channel.getMessageHistory(1).bulkDelete();
-				channel.sendMessage("Current users: " + event.getGuild().getUsers().size());
+				channel.changeTopic("Current users: " + event.getGuild().getUsers().size());
 			}
 		}
 	}
